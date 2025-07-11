fce58b8e0240ed9840a1d59671360a9c6ceef1ec
Vaibhav Goswami <148686075+vaibhavgoswami10@users.noreply.github.com>
2025-07-11 21:34:39 +0530

    Add files via upload

---
+++ Week 4/Mandatory Hands-on 1 & 2/spring-learn/src/main/java/com/cognizant/spring_learn/HomeController.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Spring Learn!";
+    }
+}
