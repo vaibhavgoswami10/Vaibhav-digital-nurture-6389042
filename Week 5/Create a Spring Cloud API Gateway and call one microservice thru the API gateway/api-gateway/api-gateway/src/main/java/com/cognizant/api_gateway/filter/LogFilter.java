503a73f26307b361c22d282d92350e4a99359faf
Vaibhav Goswami <148686075+vaibhavgoswami10@users.noreply.github.com>
2025-07-18 01:46:57 +0530

    Add files via upload

---
+++ Week 5/Create a Spring Cloud API Gateway and call one microservice thru the API gateway/api-gateway/api-gateway/src/main/java/com/cognizant/api_gateway/filter/LogFilter.java
@@ -0,0 +1,22 @@
+package com.cognizant.api_gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(1)
+public class LogFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+        logger.info("Incoming Request: {}", exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
+}
